a296762284ae38a56364e859e2e6ba994ca57199
Luke <rohenaz@gmail.com>
2020-09-16 22:39:32 -0400

    remove comment

--- sign_test.go
+++ sign_test.go
@@ -12,8 +12,6 @@ func TestSignMessage(t *testing.T) {
 	// privKey string, message string, compress bool
 	sig := SignMessage(privKey, "Testing!")
 
-	t.Log("got sig", sig)
-
 	if sig != "IBDscOd/Ov4yrd/YXantqajSAnW4fudpfr2KQy5GNo9pZybF12uNaal4KI822UpQLS/UJD+UK2SnNMn6Z3E4na8=" {
 		t.Error("Failed to generate expected signature", sig)
 	}
