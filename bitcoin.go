10f4e7e4d1a9984993edcab19416622f4dac2e2f
mrz1836 <mrz1818@gmail.com>
2020-10-02 11:13:35 -0400

    Added package docs

---
+++ bitcoin.go
@@ -0,0 +1,7 @@
+// Package bitcoin is a small collection of utility functions for working with Bitcoin (BSV)
+//
+// If you have any suggestions or comments, please feel free to open an issue on
+// this GitHub repository!
+//
+// By BitcoinSchema Organization (https://bitcoinschema.org)
+package bitcoin
